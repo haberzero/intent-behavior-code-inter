0efb2ba98d80ff235bdefc57124f6d3e643cea2f
Nan Shi (施楠) <42118595+haberzero@users.noreply.github.com>
2026-03-23 00:03:30 +0800

    test(compiler/semantic): 扩展 SemanticAnalyzer 测试覆盖
    新增覆盖:
    - visit_IbCall: 函数调用、方法调用、嵌套调用
    - visit_IbAttribute: 属性访问、属性赋值
    - visit_IbSubscript: 下标访问
    - visit_IbAugAssign: 增强赋值 (+=, -=, *=)
    - visit_IbTry/IbExceptHandler: 异常处理
    - visit_IbRaise/IbRetry: 异常抛出和重试
    - visit_IbExprStmt: 表达式语句
    - visit_IbFor: for循环带赋值
    - visit_IbCompare: 链式比较、比较运算
    - visit_IbBoolOp: 布尔运算 (and/or/not)
    - 侧表统计验证 (node_scenes, node_to_type, node_to_symbol)
    
    SemanticAnalyzer 测试: 31 -> 66

--- tests/compiler/semantic/test_semantic_analyzer.py
+++ tests/compiler/semantic/test_semantic_analyzer.py
@@ -147,3 +147,143 @@ class TestSemanticAnalyzer(unittest.TestCase):
     def test_analyze_var_ref(self):
         result, tracker, analyzer, compilation = self._analyze("$variable")
         self.assertGreaterEqual(len(result.body), 1)
+
+    def test_analyze_function_call(self):
+        result, tracker, analyzer, compilation = self._analyze("func foo():\n    pass\nfoo()\n")
+        self.assertGreaterEqual(len(result.body), 2)
+
+    def test_analyze_method_call(self):
+        result, tracker, analyzer, compilation = self._analyze("func foo():\n    pass\nobj.foo()\n")
+        self.assertGreaterEqual(len(result.body), 2)
+
+    def test_analyze_attribute_access(self):
+        result, tracker, analyzer, compilation = self._analyze("func foo():\n    pass\nobj.attr\n")
+        self.assertGreaterEqual(len(result.body), 2)
+
+    def test_analyze_subscript_access(self):
+        result, tracker, analyzer, compilation = self._analyze("func foo():\n    pass\narr[0]\n")
+        self.assertGreaterEqual(len(result.body), 2)
+
+    def test_analyze_augmented_assignment_add(self):
+        result, tracker, analyzer, compilation = self._analyze("x = 1\nx += 1\n")
+        self.assertEqual(len(result.body), 2)
+
+    def test_analyze_augmented_assignment_subtract(self):
+        result, tracker, analyzer, compilation = self._analyze("x = 1\nx -= 1\n")
+        self.assertEqual(len(result.body), 2)
+
+    def test_analyze_augmented_assignment_multiply(self):
+        result, tracker, analyzer, compilation = self._analyze("x = 2\nx *= 2\n")
+        self.assertEqual(len(result.body), 2)
+
+    def test_analyze_try_except(self):
+        result, tracker, analyzer, compilation = self._analyze("try:\n    x = 1\nexcept:\n    pass\n")
+        self.assertEqual(len(result.body), 1)
+
+    def test_analyze_raise(self):
+        result, tracker, analyzer, compilation = self._analyze("func foo():\n    raise Exception()\n")
+        self.assertEqual(len(result.body), 1)
+
+    def test_analyze_retry(self):
+        result, tracker, analyzer, compilation = self._analyze("retry\n")
+        self.assertGreaterEqual(len(result.body), 1)
+
+    def test_analyze_expr_stmt(self):
+        result, tracker, analyzer, compilation = self._analyze("func foo():\n    pass\nfoo()\n")
+        self.assertEqual(len(result.body), 2)
+
+    def test_analyze_class_with_init_and_attrs(self):
+        result, tracker, analyzer, compilation = self._analyze("class User:\n    func __init__(str n, int a):\n        self.name = n\n")
+        self.assertEqual(len(result.body), 1)
+
+    def test_analyze_nested_call(self):
+        result, tracker, analyzer, compilation = self._analyze("func foo():\n    return 1\nfoo() + foo()\n")
+        self.assertGreaterEqual(len(result.body), 2)
+
+    def test_analyze_type_annotated_expr(self):
+        result, tracker, analyzer, compilation = self._analyze("func foo(int x):\n    pass\n")
+        self.assertEqual(len(result.body), 1)
+
+    def test_analyze_if_else_expression(self):
+        result, tracker, analyzer, compilation = self._analyze("if True:\n    x = 1\nelse:\n    x = 2\n")
+        self.assertEqual(len(result.body), 1)
+
+    def test_analyze_for_loop_with_assignment(self):
+        result, tracker, analyzer, compilation = self._analyze("for i in items:\n    x = i\n")
+        self.assertEqual(len(result.body), 1)
+
+    def test_analyze_side_table_node_scenes_count(self):
+        result, tracker, analyzer, compilation = self._analyze("if True:\n    x = 1\nwhile True:\n    y = 2\n")
+        self.assertGreater(len(compilation.node_scenes), 0)
+
+    def test_analyze_side_table_node_types_count(self):
+        result, tracker, analyzer, compilation = self._analyze("x = 42\ny = \"hello\"\n")
+        self.assertGreater(len(compilation.node_to_type), 0)
+
+    def test_analyze_side_table_node_to_symbol_count(self):
+        result, tracker, analyzer, compilation = self._analyze("x = 42\nfunc foo():\n    pass\n")
+        self.assertGreater(len(compilation.node_to_symbol), 0)
+
+    def test_analyze_return_in_function(self):
+        result, tracker, analyzer, compilation = self._analyze("func add(int a, int b) -> int:\n    return a + b\n")
+        self.assertEqual(len(result.body), 1)
+
+    def test_analyze_function_call_no_args(self):
+        result, tracker, analyzer, compilation = self._analyze("func greet():\n    pass\ngreet()\n")
+        self.assertEqual(len(result.body), 2)
+
+    def test_analyze_function_call_with_args(self):
+        result, tracker, analyzer, compilation = self._analyze("func add(int a, int b):\n    pass\nadd(1, 2)\n")
+        self.assertEqual(len(result.body), 2)
+
+    def test_analyze_break_in_while(self):
+        result, tracker, analyzer, compilation = self._analyze("while True:\n    break\n")
+        self.assertEqual(len(result.body), 1)
+
+    def test_analyze_continue_in_while(self):
+        result, tracker, analyzer, compilation = self._analyze("while True:\n    continue\n")
+        self.assertEqual(len(result.body), 1)
+
+    def test_analyze_while_with_else(self):
+        result, tracker, analyzer, compilation = self._analyze("while False:\n    pass\n")
+        self.assertEqual(len(result.body), 1)
+
+    def test_analyze_assignment_from_call(self):
+        result, tracker, analyzer, compilation = self._analyze("func get_value() -> int:\n    return 42\nresult = get_value()\n")
+        self.assertEqual(len(result.body), 2)
+
+    def test_analyze_assignment_from_attribute(self):
+        result, tracker, analyzer, compilation = self._analyze("func get_obj():\n    return None\nval = get_obj().attr\n")
+        self.assertEqual(len(result.body), 2)
+
+    def test_analyze_assignment_from_subscript(self):
+        result, tracker, analyzer, compilation = self._analyze("func get_list():\n    return None\nval = get_list()[0]\n")
+        self.assertEqual(len(result.body), 2)
+
+    def test_analyze_binary_and_or(self):
+        result, tracker, analyzer, compilation = self._analyze("True and False or True\n")
+        self.assertGreaterEqual(len(result.body), 1)
+
+    def test_analyze_unary_not(self):
+        result, tracker, analyzer, compilation = self._analyze("not True\n")
+        self.assertGreaterEqual(len(result.body), 1)
+
+    def test_analyze_compare_chained(self):
+        result, tracker, analyzer, compilation = self._analyze("a < b < c\n")
+        self.assertGreaterEqual(len(result.body), 1)
+
+    def test_analyze_compare_eq_ne(self):
+        result, tracker, analyzer, compilation = self._analyze("a == b\na != b\n")
+        self.assertEqual(len(result.body), 2)
+
+    def test_analyze_compare_gt_lt(self):
+        result, tracker, analyzer, compilation = self._analyze("x > y\nx < y\n")
+        self.assertEqual(len(result.body), 2)
+
+    def test_analyze_compare_ge_le(self):
+        result, tracker, analyzer, compilation = self._analyze("x >= y\nx <= y\n")
+        self.assertEqual(len(result.body), 2)
+
+    def test_analyze_assignment_with_type_annotation(self):
+        result, tracker, analyzer, compilation = self._analyze("x = 42\n")
+        self.assertEqual(len(result.body), 1)
