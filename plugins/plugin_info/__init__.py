853dee6264f398a6afd2085f0c9d03e7253bc21e
Nan Shi (施楠) <42118595+haberzero@users.noreply.github.com>
2026-03-25 22:16:14 +0800

    chore(cleanup): 清理重构过程中遗留的冗余插件目录

--- plugins/plugin_info/__init__.py
+++
@@ -1,8 +0,0 @@
-class PluginMetadata:
-    def __init__(self):
-        self.version = "2.0.0"
-        self.author = "IBCI-Inter Team"
-        # [SECURITY] IES 2.0 要求显式声明属性白名单
-        self._ibci_whitelist = ["version", "author"]
-
-implementation = PluginMetadata()
