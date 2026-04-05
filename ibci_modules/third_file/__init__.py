cb45a06abc0064d00a0bf096157c732ab3c9bc77
哈伯 <yunnan_shinan@qq.com>
2026-04-05 18:16:12 +0800

    chore: 删除third_file插件（已合并到file插件）

--- ibci_modules/third_file/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Third File Analysis Plugin Entry Point
-Minimalist entry point that delegates to core logic.
-"""
-from .core import create_implementation
-
-__all__ = ["create_implementation"]
